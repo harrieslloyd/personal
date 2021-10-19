2a3fe7940592b0237f620fa79e77acdc66501c3d
Harries Lloyd <92803503+harrieslloyd@users.noreply.github.com>
2021-10-19 18:02:04 +0000

    Cleaned Up & added credits

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function getUrl(url) {
-  fetch(url)
-  .then(function() {
+const url = 'https://api.nasa.gov/planetary/apod?api_key=A2PqnJVXsBkKFfUKUojEE1h6VaXpHlw2UKL8huIq';
 
+fetch(url)
+.then(function(data) {
+    document.getElementById("img").src = data.url
+    document.getElementById("text").innerHTML = data.explanation
   })
-  .catch(function() {
-    
+    .catch(function(error) {
+    console.log(error);
   });
-}
