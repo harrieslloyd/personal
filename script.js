8f4ba931d46af715ed07635d9a1786951290405e
Harries Lloyd <92803503+harrieslloyd@users.noreply.github.com>
2021-10-19 18:14:36 +0000

    Fixed some stuff

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const url = 'https://api.nasa.gov/planetary/apod?api_key=A2PqnJVXsBkKFfUKUojEE1h6VaXpHlw2UKL8huIq';
-
 fetch(url)
+.then((resp) => resp.json())
 .then(function(data) {
-    document.getElementById("img").src = data.url
-    document.getElementById("text").innerHTML = data.explanation
-  })
-    .catch(function(error) {
-    console.log(error);
-  });
+  document.getElementById("img").src = data.url
+  document.getElementById("text").innerHTML = data.explanation
+})
+.catch(function(error) {
+  console.log(error);
+});
