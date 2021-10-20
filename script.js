e5f56a21f5bc463a167d101cd4b11fb125520e71
Harries Lloyd <92803503+harrieslloyd@users.noreply.github.com>
2021-10-20 16:12:49 +0000

    Working on download button

--- script.js
+++ script.js
@@ -1,28 +1,71 @@
 function fetchUrl() {
   const url = 'https://api.nasa.gov/planetary/apod?api_key=A2PqnJVXsBkKFfUKUojEE1h6VaXpHlw2UKL8huIq';
   fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-    document.getElementById("img").src = data.url;
-    document.getElementById("text").innerHTML = data.explanation;
-    document.getElementById("downloadbuttonlink").download = data.url;
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+    .then((resp) => resp.json())
+    .then(function (data) {
+      document.getElementById("img").src = data.url;
+      document.getElementById("text").innerHTML = data.explanation;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
+function getLinkAndDownload() {
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=A2PqnJVXsBkKFfUKUojEE1h6VaXpHlw2UKL8huIq';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      download(data.url)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
-window.transitionToPage = function(href) {
-    document.querySelector('body').style.opacity = 0
-    setTimeout(function() { 
-        window.location.href = href
-    }, 500)
+function download(url) {
+  const a = document.createElement('a')
+  a.href = url
+  a.download = url.split('/').pop()
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+}
+
+window.transitionToPage = function (href) {
+  document.querySelector('body').style.opacity = 0
+  setTimeout(function () {
+    window.location.href = href
+  }, 500)
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+function download(file, text) {
+  //creating an invisible element
+  var element = document.createElement('a');
+  element.setAttribute('href',
+    'data:text/plain;charset=utf-8, '
+    + encodeURIComponent(text));
+  element.setAttribute('download', file);
+
+  // Above code is equivalent to
+  // <a href="path of file" download="file name">
+
+  document.body.appendChild(element);
+
+  //onClick property
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+
+
+
+
 
 let pages = [
   'nasa.html',
@@ -30,16 +73,16 @@ let pages = [
   //'snake.html'
 ]
 
-window.transitionToRandomPage = function() {
-    document.querySelector('body').style.opacity = 0
-    let href = pages[getRandomInt(pages.length)]
-    setTimeout(function() { 
-        window.location.href = href
-    }, 500)
+window.transitionToRandomPage = function () {
+  document.querySelector('body').style.opacity = 0
+  let href = pages[getRandomInt(pages.length)]
+  setTimeout(function () {
+    window.location.href = href
+  }, 500)
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1
+document.addEventListener('DOMContentLoaded', function (event) {
+  document.querySelector('body').style.opacity = 1
 })
 
 function sleep(ms) {
